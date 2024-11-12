df2975186429d82a65c5e9122780df4fe5b3e225
krishna21s <venkatakrishnac60@gmail.com>
2024-11-12 18:19:46 +0530

    changed the mistake in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/stpcone_2025/',
+  base: '/stepcone_2025/',
   build: {
     outDir: 'build',  // Change output directory to 'build'
   },
