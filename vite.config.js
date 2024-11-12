a3af16a58d35376969266df1c0405786a3bd3e74
krishna21s <venkatakrishnac60@gmail.com>
2024-11-12 18:07:45 +0530

    changed something

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/stpcone_2025/'
+  base: '/stpcone_2025/',
+  build: {
+    outDir: 'build',  // Change output directory to 'build'
+  },
 })
